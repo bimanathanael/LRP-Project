346af79cb0557b72952edb992afedc992ccf06c8
bimanathanael95@gmai.com <bimanathanael95@gmail.com>
2020-09-14 17:00:07 +0700

    function register done

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,31 @@
+const firebase = require('firebase')
+var firebaseConfig = {
+  apiKey: "AIzaSyCCrVRA9RMnrrlSX5ZppDKlcek2QdUyVXc",
+  authDomain: "lrp-project-b0a46.firebaseapp.com",
+  databaseURL: "https://lrp-project-b0a46.firebaseio.com",
+  projectId: "lrp-project-b0a46",
+  storageBucket: "lrp-project-b0a46.appspot.com",
+  messagingSenderId: "638126377204",
+  appId: "1:638126377204:web:e46c74711b3bf12468b6a1"
+};
+
+firebase.initializeApp(firebaseConfig);
+
 class Controllers {
-  static welcome( req, res ) {
-    res.status(200).json("hello")
+  static register( req, res ) {
+    writeUserData(4,req.body.email, req.body.password)
+  
+    function writeUserData(userId,email, password) {
+      firebase.database().ref('users/' + userId).set({
+        password: password,
+        email: email,
+      });
+    }
+
+    return res.status(201).json("success register")
   }
+
+  
 }
 
 module.exports = Controllers
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const controllers = require('../controllers')
 
-router.get('/', controllers.welcome)
+router.post('/register', controllers.register)
+router.post('/login', controllers.login)
 
 module.exports = router
