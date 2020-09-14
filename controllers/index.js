33a30adc78ea099003cc7d041496d5a6648a7018
bimanathanael95@gmai.com <bimanathanael95@gmail.com>
2020-09-14 21:15:11 +0700

    fix register bug

--- controllers/index.js
+++ controllers/index.js
@@ -6,25 +6,14 @@ const validator = require("email-validator");
 firebase.initializeApp(firebaseConfig);
 
 class Controllers {
-  static checkUser (){
-    let users = firebase.database().ref('/users/')
-    let totalUser = 0
-    users.on('value', async (allData) => {
-      totalUser = await Object.keys(allData.val()).length
-      console.log(totalUser, "<<<<")
-      return totalUser
-    })
-  }
-
   static register( req, res ) {
     if(validator.validate(req.body.email) == false) {
       return res.status(404).json({message: "email not valid"})
     }
-    let totalUser = Controllers.checkUser()
-    writeUserData(totalUser,req.body.email, req.body.password, req.body.name, req.body.address)
+    writeUserData(req.body.email, req.body.password, req.body.name, req.body.address)
 
-    function writeUserData(user,email, password,name, address) {
-      firebase.database().ref('users/' + 2).set({
+    function writeUserData(email, password,name, address) {
+      firebase.database().ref('users/').push().set({
         password: password,
         email: email,
         name: name,
@@ -38,8 +27,8 @@ class Controllers {
     let isFound = false
     const oneData = firebase.database().ref('/users/')
     oneData.on('value', (allData) => {
-      allData.val().forEach( data => {
-        if(data.email == req.body.email && data.password == req.body.password){
+      for (const data in allData.val()) {
+        if(allData.val()[data].email == req.body.email && allData.val()[data].password == req.body.password){
           isFound = true
           let userData = {
             email: req.body.email
@@ -47,7 +36,7 @@ class Controllers {
           let tokenJwt = encode(userData)
           return res.status(200).json({ access_token: tokenJwt})
         }
-      })
+      }
       if(isFound == false){
         return res.status(404).json({message: "login data not found"})
       } 
@@ -60,11 +49,11 @@ class Controllers {
       
       const oneData = firebase.database().ref('/users/')
       oneData.on('value', (allData) => {
-        allData.val().forEach( data => {
-          if(data.email == userData.email){
-            return res.status(200).json(data)
+        for (const data in allData.val()) {
+          if(allData.val()[data].email == userData.email){
+            return res.status(200).json(allData.val()[data])
           }
-        })
+        }
       }, function (errorObject) {
         return res.status(404).json(errorObject)
       });
